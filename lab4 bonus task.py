bbff69fa52783b940d1159157801226a1d08da5d
Adibayeva04 <146876719+Adibayeva04@users.noreply.github.com>
2023-10-20 22:43:03 +0600

    Update lab4 bonus task.py

--- lab4 bonus task.py
+++ lab4 bonus task.py
@@ -1,49 +1,25 @@
-def count_occurrences(input_tuple):
-    element_count = {}  # Dictionary to store element counts
-    for item in input_tuple:
-        if item in element_count:
-            element_count[item] += 1
-        else:
-            element_count[item] = 1
+# Запрашиваем у пользователя ввод текста, который нужно зашифровать.
+text = input("Введите текст для шифрования: ")
 
-    result_tuple = []
-    for item, count in element_count.items():
-        result_tuple.append((item, count))
+# Запрашиваем у пользователя ввод сдвига, который будет использован для шифрования.
+shift = int(input("Введите сдвиг: "))
 
-    return tuple(result_tuple)
+# Инициализируем пустую строку для сохранения зашифрованного текста.
+encrypted_text = ""
 
-
-def find_popular_elements(input_tuple):
-    element_count = count_occurrences(input_tuple)
-    max_count = max(element_count, key=lambda x: x[1])[1]  # Find the maximum count
-    min_count = min(element_count, key=lambda x: x[1])[1]  # Find the minimum count
-
-    most_popular = [item[0] for item in element_count if item[1] == max_count]
-    least_popular = [(item[0], item[1]) for item in element_count if item[1] == min_count]
-
-    frequent_elements = [item for item in element_count if item[1] == max_count]
-
-    return most_popular, least_popular, frequent_elements
-
-
-def print_results(input_tuple, most_popular, least_popular, frequent_elements):
-    print("Elements and their occurrences:")
-    print(count_occurrences(input_tuple))
-
-    if len(most_popular) == 1:
-        print(f"The most popular element: {most_popular[0]} - {max_count} occurrences")
+# Начинаем цикл, который перебирает каждый символ в исходном тексте.
+for char in text:
+    if char.isalpha():
+        # Если символ - буква, проверяем, является ли она заглавной или строчной.
+        # Затем вычисляем зашифрованный символ с использованием шифра Цезаря.
+        shifted_char = chr((ord(char) - 65 + shift) % 26 + 65) if char.isupper() else chr((ord(char) - 97 + shift) % 26 + 97)
+        encrypted_text += shifted_char  # Добавляем зашифрованный символ к зашифрованному тексту.
     else:
-        print("The most popular element: no most popular element")
+        encrypted_text += char  # Если символ не является буквой, оставляем его без изменений.
 
-    if len(least_popular) == 1:
-        print(f"The least popular element: {least_popular[0][0]} - {min_count} occurrence(s)")
-    else:
-        print("The least popular element: no least popular element")
+# Выводим зашифрованный текст.
+print("Зашифрованный текст:", encrypted_text)
 
-    print("The frequent elements:", frequent_elements)
-
-
-# Sample Input
 input_data = (5, 55, 10, 1, 0, 1, 55, 77, 10, 5, 5, 55, 77)
 element_count = count_occurrences(input_data)
 
