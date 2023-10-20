e370ab124f365042fb520f2dfb5a991377135574
Adibayeva04 <146876719+Adibayeva04@users.noreply.github.com>
2023-10-20 22:47:16 +0600

    Update lab4-3.py

--- lab4-3.py
+++ lab4-3.py
@@ -2,12 +2,16 @@ try:
     # Sample Input
     tuple_A = (1, 2, 3, 4, 5, 6, 7)
     tuple_B = (5, 6, 7, 9, 7, 1, 10, 10)
-    # Find the midpoint for both tuples
+
+    # Находим среднюю позицию для обоих кортежей
     midpoint_A = len(tuple_A) // 2
     midpoint_B = len(tuple_B) // 2
-    # Concatenate the first half of A with the second half of B
+
+    # Сцепляем первую половину кортежа A с второй половиной кортежа B
     final_tuple = tuple_A[:midpoint_A] + tuple_B[midpoint_B:]
-    # Print the concatenated tuple
+
+    # Выводим объединенный кортеж
     print(final_tuple)
+
 except ValueError:
-    print("Ввод неверный")
+    print("Ввод неверный")  # Если возникнет ошибка типа ValueError, выводим сообщение об ошибке
